52e2f7367674555b58739b812f148f5254333693
Max Gasner <max@priorknowledge.com>
2012-10-21 12:21:41 -0700

    rename to avoid conflict with stdio

--- ch1/longestline.c
+++ ch1/longestline.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum line length */
 
-int getline(char line[], int maxline);
+int mygetline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print the longest input line */
@@ -13,7 +13,7 @@ main()
 	char longest[MAXLINE]; /* longest line saved here */
 
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0)
+	while ((len = mygetline(line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
 			copy(longest, line);
@@ -23,15 +23,15 @@ main()
 	return 0;
 }
 
-/* getline: read a line into s, return length */
-int getline(char s[], int lim)
+/* mygetline: read a line into s, return length */
+int mygetline(char s[], int lim)
 {
 	int c, i;
 	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
 		s[i] = c;
 	if (c == '\n') {
 		s[i] = c;
-		++i
+		++i;
 	}
 	s[i] = '\0';
 	return i;
